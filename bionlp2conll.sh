85459390c035802091c44606c039a8d2b0b025ae
Lenz <Lenz.Furrer@gmail.com>
2019-07-18 18:07:13 +0200

    include extension datasets

--- bionlp2conll.sh
+++ bionlp2conll.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Arg 1: One of {CHEBI,CL,GO_{BP,CC,MF},MOP,NCBITaxon,PR,SO,UBERON}
+# Arg 1: One of {CHEBI,CL,GO_{BP,CC,MF},MOP,NCBITaxon,PR,SO,UBERON}{,_EXT}
 res=$1
-# Arg 2: Target directory
+# Arg 2: Target directory (absolute path!)
 target=$2
 # Additional args: passed to standoff2conll.py.
 
@@ -10,12 +10,14 @@ CRAFT="$(pwd)/CRAFT"
 s2c="$(pwd)/standoff2conll/standoff2conll.py"
 
 tdir=$(mktemp -d)
+res_base=${res%_EXT}
+res_sub=${res/_EXT/+extensions}
 
 cd $tdir
 ln -s -t . ${CRAFT}/articles/txt/*.txt
-for fn in ${CRAFT}/concept-annotation/$res/$res/bionlp/*; do
+for fn in ${CRAFT}/concept-annotation/$res_base/$res_sub/bionlp/*; do
 	base=$(basename $fn .bionlp)
 	ln -s $fn $base.ann
-	$s2c -s IOBES -c "${@:3}" ./ > $target/$base.conll
+	$s2c -s IOBES -c -d first-span "${@:3}" ./ > $target/$base.conll
 	rm $base.ann
 done
