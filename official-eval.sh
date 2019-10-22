a4afc5d0be25788ed972ce56d06e17a6c3c93367
Lenz <Lenz.Furrer@gmail.com>
2019-10-22 09:18:26 +0200

    eval-suite wrapper: stop early if format conversion fails

--- official-eval.sh
+++ official-eval.sh
@@ -8,8 +8,8 @@ c2s="standoff2conll/conll2standoff.py"
 for path in "$@"; do
 	tgtdir="$evaldir/$(basename $path)"
 	mkdir "$tgtdir"
-	ls "$path"/*.conll | parallel -j $workers "$c2s < {} > $tgtdir/{/.}.bionlp"
-done
+	ls "$path"/*.conll | parallel -j $workers --halt now,fail=1 "$c2s < {} > $tgtdir/{/.}.bionlp"
+done || exit
 
 docker run --rm -v $evaldir:/files-to-evaluate ucdenverccp/craft-eval:3.1.3_0.1.2 \
 	sh -c '(cd /home/craft/evaluation && boot javac eval-concept-annotations)'
